c88de5f52feab8d8d7197366f6b5b2d48d0508d0
Ovus Iroanwusi <oiroanwusi1@nait.ca>
2024-10-02 23:53:14 -0600

    testing commit

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ displayButton.addEventListener("click", () =>{
 });
 
 
-const links = document.querySelectorAll('.javascript-resources a');
+// const links = document.querySelectorAll('.javascript-resources a');
 
-links.forEach(link => {
-  link.addEventListener('mouseover', (event) => {
-    console.log(event.target);
-    event.target.classList.add('fw-bold');
-  });
+// links.forEach(link => {
+//   link.addEventListener('mouseover', (event) => {
+//     console.log(event.target);
+//     event.target.classList.add('fw-bold');
+//   });
 
-  link.addEventListener('mouseout', (event) => {
-    event.target.classList.remove('fw-bold');
-  });
+//   link.addEventListener('mouseout', (event) => {
+//     event.target.classList.remove('fw-bold');
+//   });
 
-  link.addEventListener('click', (event) => {
-    event.target.classList.add('fst-italic');
-  });
-});
+//   link.addEventListener('click', (event) => {
+//     event.target.classList.add('fst-italic');
+//   });
+// });
